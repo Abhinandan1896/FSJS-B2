d9b17433fdc39767fdbde25ae2f6bc0ecae8deed
Abhi <kawtheabhinandan@gmail.com>
2023-01-17 00:26:47 +0530

    Q1 updated

---
+++ JavaScript Assignment/1 Question/variable.js
@@ -0,0 +1,34 @@
+//data typs- it is type of data/value i.e is it integer, string , boolean etc
+
+//1)String Datatype
+let nam='Abhinandan';
+console.log(nam);
+console.log("datatype of nam is",typeof nam);
+
+//2)Interger datatype
+const mobnum=9168035300;
+console.log(mobnum);
+console.log("datatype of mobnum is",typeof mobnum);
+
+//3)boolean datatype(true or false)
+let a=10;
+let b=12;
+console.log("a=",a,"","b=",b);
+console.log("a==b is",a==b);
+console.log("b>a is",b>a);
+console.log("datatype of b>a is",b>a);
+
+
+//Null-special keyword denoting a null value
+//In JavaScript, null is a special value that represents an empty or unknown value.
+//The code above suggests that the number variable is empty at the moment and may have a value later.
+
+let number = null;
+console.log(number);
+console.log("datatype of null is",typeof number)
+
+
+//Undefined-If a variable is declared but the value is not assigned, then the value of that variable will be undefined. 
+let nam1;
+console.log(nam1);
+console.log("datatype of nam1 is",typeof nam1);
