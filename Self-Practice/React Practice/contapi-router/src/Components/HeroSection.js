6d3f6b2272d8eeff6fa7d6f2f9d9977cf580e412
Abhi <kawtheabhinandan@gmail.com>
2023-05-16 23:47:44 +0530

    content-api and router practice

---
+++ Self-Practice/React Practice/contapi-router/src/Components/HeroSection.js
@@ -0,0 +1,13 @@
+import UserCard from "./UserCard"
+
+
+const HeroSection=()=>{
+    return(
+        <div>
+            <img src="https://picsum.photos/200" alt="Random Scene"/>
+            <UserCard/>
+        </div>
+    )
+}
+
+export default HeroSection
---
+++ Self-Practice/React Practice/contapi-router/src/Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Navbar=()=>{
+    return(
+        <h2>I Am Navigation</h2>
+    )
+}
+
+export default Navbar
